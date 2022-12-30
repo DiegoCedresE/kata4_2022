ec0a53bdcb1ee00702c57129d7cf2e8d6c1a647d
diegocedres <diegocedres@mbp-de-diego.home>
2022-12-30 18:50:13 +0000

    Kata 4

---
+++ src/kata4/kata4.java
@@ -0,0 +1,39 @@
+package kata4;
+
+import java.io.IOException;
+import kata4.view.HistogramDisplay;
+import kata4.view.MailListReader;
+import kata4.view.MailHistogramBuilder;
+import kata4.model.Histogram;
+import kata4.model.Mail;
+import java.util.List;
+
+public class kata4 {
+    Histogram<String> histogram;
+    List<Mail> mailList;
+ 
+    public static void main(String[] args) {
+        kata4 kata4 = new kata4();
+        kata4.execute();
+    }
+    
+    private void execute() {
+        input();
+        process();
+        output();
+    }
+
+    private void input() {
+        String fileName = new String("email.txt");
+        mailList = MailListReader.read(fileName);
+    }
+
+    private void process() {
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    private void output() {
+        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM", histogram);
+        histogramDisplay.execute(); 
+    }
+}
